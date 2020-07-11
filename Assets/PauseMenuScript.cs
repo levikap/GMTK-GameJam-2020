6f5b8c29a894afd6b43a350c3a487ef0b181f27e
Kimin Lee <kiminlee2000225@gmail.com>
2020-07-11 19:52:45 -0400

    pause works

--- Assets/PauseMenuScript.cs
+++ Assets/PauseMenuScript.cs
@@ -1,18 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool isGamePaused = false;
+    public GameObject pauseMenu;
+    public GameObject bg;
+
+    private void Start()
     {
-        
+        isGamePaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!GameState.isGameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isGamePaused)
+                {
+                    ResumeGame();
+                }
+                else
+                {
+                    PauseGame();
+                }
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        isGamePaused = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PauseGameForLoadingScreen()
+    {
+        isGamePaused = true;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        isGamePaused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
