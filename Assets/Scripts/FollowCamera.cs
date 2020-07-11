65eb906c653068c84dded6c95185fc1556da96c4
levikap <leevzofrohan@gmail.com>
2020-07-11 15:08:42 -0700

    level one reset

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,8 +7,12 @@ public class FollowCamera : MonoBehaviour
     public float interpVelocity;
     public float minDistance;
     public float followDistance;
+
     private GameObject target;
+    public float smoothSpeed = 10f;
     public Vector3 offset;
+
+
     Vector3 targetPos;
 
     void Start()
@@ -22,16 +26,20 @@ public class FollowCamera : MonoBehaviour
     {
         if (target)
         {
-            Vector3 posNoZ = transform.position;
-            posNoZ.z = target.transform.position.z;
+            //Vector3 posNoZ = transform.position;
+            //posNoZ.z = target.transform.position.z;
 
-            Vector3 targetDirection = (target.transform.position - posNoZ);
+            //Vector3 targetDirection = (target.transform.position - posNoZ);
 
-            interpVelocity = targetDirection.magnitude * 5f;
+            //interpVelocity = targetDirection.magnitude * 5f;
 
-            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+            //targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
 
-            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+            //transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+            Vector3 desiredPosition = target.transform.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
 
         }
     }
