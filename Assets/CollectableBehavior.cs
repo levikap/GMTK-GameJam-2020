0224f61e5ae1b882ef310b0ca95fa799268e5bf6
Kimin Lee <kiminlee2000225@gmail.com>
2020-07-10 21:28:59 -0400

    changing collectable state

--- Assets/CollectableBehavior.cs
+++ Assets/CollectableBehavior.cs
@@ -9,11 +9,43 @@ public class CollectableBehavior : MonoBehaviour
     public static int pickedUpCount = 0;
     public GameObject gs;
 
+    private GameObject[] cookies;
+    private GameObject[] stars;
+
     // Start is called before the first frame update
     void Start()
     {
         ++maxPickUpCount;
         pickedUpCount = 0;
+
+        stars = GameObject.FindGameObjectsWithTag("CollectableStar");
+        cookies = GameObject.FindGameObjectsWithTag("CollectableCookie");
+    }
+
+    void Update()
+    {
+        SwitchController.isAwake = false;
+        if (SwitchController.isAwake)
+        {
+            foreach(GameObject cookie in cookies)
+            {
+                cookie.SetActive(true);
+            }
+            foreach (GameObject star in stars)
+            {
+                star.SetActive(false);
+            }
+        } else
+        {
+            foreach (GameObject cookie in cookies)
+            {
+                cookie.SetActive(false);
+            }
+            foreach (GameObject star in stars)
+            {
+                star.SetActive(true);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
--- Assets/EnemyBehavior.cs
+++ Assets/EnemyBehavior.cs
@@ -7,9 +7,40 @@ public class EnemyBehavior : MonoBehaviour
     //public AudioClip pickupSFX;
     public GameObject gs;
 
+    private GameObject[] vegetables;
+    private GameObject[] monsters;
+
     // Start is called before the first frame update
     void Start()
     {
+        vegetables = GameObject.FindGameObjectsWithTag("EnemyVegetable");
+        monsters = GameObject.FindGameObjectsWithTag("EnemyMonster");
+    }
+
+    void Update()
+    {
+        if (SwitchController.isAwake)
+        {
+            foreach (GameObject vegetable in vegetables)
+            {
+                vegetable.SetActive(true);
+            }
+            foreach (GameObject monster in monsters)
+            {
+                monster.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (GameObject vegetable in vegetables)
+            {
+                vegetable.SetActive(false);
+            }
+            foreach (GameObject monster in monsters)
+            {
+                monster.SetActive(true);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
