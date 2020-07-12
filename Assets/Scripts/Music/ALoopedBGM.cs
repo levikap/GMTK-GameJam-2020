67f3671ba3225cc35590b3a79c2587e617e3ba12
DThaiPome <r.gehrlein154@gmail.com>
2020-07-11 23:14:20 -0400

    Uploaded music WAV's, added music looper and a component that plays and loops a single track.

---
+++ Assets/Scripts/Music/ALoopedBGM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ALoopedBGM : MonoBehaviour
+{
+    public abstract void play();
+
+    public abstract void stop();
+
+    public abstract void fadeIn();
+
+    public abstract void fadeOut();
+
+    public abstract void setSong(AudioClip song, float volume, float loopSeconds);
+}
---
+++ Assets/Scripts/Music/SingleBGMPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleBGMPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject loopedBGMPrefab;
+    [SerializeField]
+    private AudioClip song;
+    [SerializeField]
+    private float volume;
+    [SerializeField]
+    private float secondsUntilLoopStarts;
+
+    private ALoopedBGM bgmPlayer;
+
+    void Start()
+    {
+        this.bgmPlayer = this.instantiateBGMPlayer();
+        this.bgmPlayer.setSong(song, this.volume, this.secondsUntilLoopStarts);
+        this.bgmPlayer.play();
+    }
+
+    private ALoopedBGM instantiateBGMPlayer()
+    {
+        GameObject g = Instantiate(this.loopedBGMPrefab);
+        g.transform.SetParent(this.transform);
+        return g.GetComponent<ALoopedBGM>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
