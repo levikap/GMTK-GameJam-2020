dc97b447c77ab324312501ec63d5102105a80b9c
Kimin Lee <kiminlee2000225@gmail.com>
2020-07-12 00:20:01 -0400

    new enemy working

--- Assets/Scripts/CollectableFollowBehavior.cs
+++ Assets/Scripts/CollectableFollowBehavior.cs
@@ -10,6 +10,9 @@ public class CollectableFollowBehavior : MonoBehaviour
     public static GameObject[] cookies;
     public static GameObject[] stars;
 
+    public static GameObject[] cookiesLimited;
+    public static GameObject[] starsLimited;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,16 @@ public class CollectableFollowBehavior : MonoBehaviour
         {
             CollectableBehavior.maxCookiesPickUpCount++;
         }
+
+        if (gameObject.tag == "CollectableLimitedStar")
+        {
+            CollectableBehavior.maxStarsPickUpCount++;
+        }
+        if (gameObject.tag == "CollectableLimitedCookie")
+        {
+            CollectableBehavior.maxCookiesPickUpCount++;
+        }
+
         CollectableBehavior.starsPickedUpCount = 0;
         CollectableBehavior.cookiesPickedUpCount = 0;
     }
@@ -29,6 +42,8 @@ public class CollectableFollowBehavior : MonoBehaviour
     {
         stars = GameObject.FindGameObjectsWithTag("CollectableFollowStar");
         cookies = GameObject.FindGameObjectsWithTag("CollectableFollowCookie");
+        starsLimited = GameObject.FindGameObjectsWithTag("CollectableLimitedStar");
+        cookiesLimited = GameObject.FindGameObjectsWithTag("CollectableLimitedCookie");
         if (SwitchController.isAwake)
         {
             foreach (GameObject cookie in cookies)
@@ -38,6 +53,19 @@ public class CollectableFollowBehavior : MonoBehaviour
 
             }
             foreach (GameObject star in stars)
+            {
+                star.GetComponent<Renderer>().enabled = false;
+                star.GetComponent<CircleCollider2D>().enabled = false;
+                Transform vegetable = star.gameObject.transform.parent.gameObject.GetComponentsInChildren<Transform>()[2];
+                star.gameObject.transform.position = vegetable.position;
+            }
+            foreach (GameObject cookie in cookiesLimited)
+            {
+                cookie.GetComponent<Renderer>().enabled = true;
+                cookie.GetComponent<CircleCollider2D>().enabled = true;
+
+            }
+            foreach (GameObject star in starsLimited)
             {
                 star.GetComponent<Renderer>().enabled = false;
                 star.GetComponent<CircleCollider2D>().enabled = false;
@@ -55,6 +83,18 @@ public class CollectableFollowBehavior : MonoBehaviour
                 cookie.gameObject.transform.position = monster.position;
             }
             foreach (GameObject star in stars)
+            {
+                star.GetComponent<Renderer>().enabled = true;
+                star.GetComponent<CircleCollider2D>().enabled = true;
+            }
+            foreach (GameObject cookie in cookiesLimited)
+            {
+                cookie.GetComponent<Renderer>().enabled = false;
+                cookie.GetComponent<CircleCollider2D>().enabled = false;
+                Transform monster = cookie.gameObject.transform.parent.gameObject.GetComponentsInChildren<Transform>()[2];
+                cookie.gameObject.transform.position = monster.position;
+            }
+            foreach (GameObject star in starsLimited)
             {
                 star.GetComponent<Renderer>().enabled = true;
                 star.GetComponent<CircleCollider2D>().enabled = true;
@@ -79,7 +119,17 @@ public class CollectableFollowBehavior : MonoBehaviour
             SoundManagerScript.PlaySound("Star");
             CollectableBehavior.starsPickedUpCount++;
         }
-        else
+        else if (gameObject.tag == "CollectableFollowCookie")
+        {
+            SoundManagerScript.PlaySound("Cookie");
+            CollectableBehavior.cookiesPickedUpCount++;
+        }
+        else if (gameObject.tag == "CollectableLimitedStar")
+        {
+            SoundManagerScript.PlaySound("Star");
+            CollectableBehavior.starsPickedUpCount++;
+        }
+        else if (gameObject.tag == "CollectableLimitedCookie")
         {
             SoundManagerScript.PlaySound("Cookie");
             CollectableBehavior.cookiesPickedUpCount++;
--- Assets/Scripts/EnemyFollowBehavior.cs
+++ Assets/Scripts/EnemyFollowBehavior.cs
@@ -8,6 +8,9 @@ public class EnemyFollowBehavior : MonoBehaviour
     public static GameObject[] vegetables;
     public static GameObject[] monsters;
 
+    public static GameObject[] vegetablesLimited;
+    public static GameObject[] monstersLimited;
+
     private GameObject player;
 
     public Animator animator;
@@ -15,9 +18,12 @@ public class EnemyFollowBehavior : MonoBehaviour
 
     public float distanceToPlayer;
 
+    private bool hitPos1;
+
     // Start is called before the first frame update
     void Start()
     {
+        hitPos1 = false;
         player = GameObject.FindGameObjectWithTag("Player");
         animator = gameObject.GetComponent<Animator>();
     }
@@ -28,6 +34,8 @@ public class EnemyFollowBehavior : MonoBehaviour
 
         vegetables = GameObject.FindGameObjectsWithTag("EnemyVegetableFollow");
         monsters = GameObject.FindGameObjectsWithTag("EnemyMonsterFollow");
+        vegetablesLimited = GameObject.FindGameObjectsWithTag("EnemyLimitedVeggie");
+        monstersLimited = GameObject.FindGameObjectsWithTag("EnemyLimitedMonster");
         if (SwitchController.isAwake)
         {
             foreach (GameObject vegetable in vegetables)
@@ -38,6 +46,39 @@ public class EnemyFollowBehavior : MonoBehaviour
                 vegetable.transform.position = Vector2.MoveTowards(vegetable.transform.position, player.transform.position, step);
             }
             foreach (GameObject monster in monsters)
+            {
+                monster.GetComponent<Renderer>().enabled = false;
+                monster.GetComponent<CircleCollider2D>().enabled = false;
+            }
+            foreach (GameObject vegetable in vegetablesLimited)
+            {
+                vegetable.GetComponent<Renderer>().enabled = true;
+                vegetable.GetComponent<CapsuleCollider2D>().enabled = true;
+                LimitedFollow limitedFollow = vegetable.GetComponent<LimitedFollow>();
+                float step = speed * Time.deltaTime;
+                //print(hitPos1);
+                print(limitedFollow.limitedPosition2.position);
+                print(limitedFollow.limitedPosition1.position);
+                if (hitPos1)
+                {
+                    vegetable.transform.position = Vector2.MoveTowards(vegetable.transform.position, limitedFollow.targetPos.position, step);
+                    if (Vector2.Distance(vegetable.transform.position, limitedFollow.targetPos.position) <= 0.2f)
+                    {
+                        hitPos1 = false;
+                        limitedFollow.targetPos = vegetable.GetComponent<LimitedFollow>().limitedPosition1;
+                    }
+                } else
+                {
+                    //print(Vector2.Distance(vegetable.transform.position, limitedFollow.targetPos.position));
+                    vegetable.transform.position = Vector2.MoveTowards(vegetable.transform.position, limitedFollow.targetPos.position, step);
+                    if (Vector2.Distance(vegetable.transform.position, limitedFollow.targetPos.position) <= 0.2f)
+                    {
+                        hitPos1 = true;
+                        limitedFollow.targetPos = vegetable.GetComponent<LimitedFollow>().limitedPosition2;
+                    }
+                }
+            }
+            foreach (GameObject monster in monstersLimited)
             {
                 monster.GetComponent<Renderer>().enabled = false;
                 monster.GetComponent<CircleCollider2D>().enabled = false;
@@ -57,6 +98,36 @@ public class EnemyFollowBehavior : MonoBehaviour
                 float step = speed * Time.deltaTime;
                 monster.transform.position = Vector2.MoveTowards(monster.transform.position, player.transform.position, step);
             }
+            foreach (GameObject vegetable in vegetablesLimited)
+            {
+                vegetable.GetComponent<Renderer>().enabled = false;
+                vegetable.GetComponent<CapsuleCollider2D>().enabled = false;
+            }
+            foreach (GameObject monster in monstersLimited)
+            {
+                monster.GetComponent<Renderer>().enabled = true;
+                monster.GetComponent<CircleCollider2D>().enabled = true;
+                LimitedFollow limitedFollow = monster.GetComponent<LimitedFollow>();
+                float step = speed * Time.deltaTime;
+                if (hitPos1)
+                {
+                    monster.transform.position = Vector2.MoveTowards(monster.transform.position, limitedFollow.targetPos.position, step);
+                    if (Vector2.Distance(monster.transform.position, limitedFollow.targetPos.position) <= 0.2f)
+                    {
+                        hitPos1 = false;
+                        limitedFollow.targetPos = monster.GetComponent<LimitedFollow>().limitedPosition1;
+                    }
+                }
+                else
+                {
+                    monster.transform.position = Vector2.MoveTowards(monster.transform.position, limitedFollow.targetPos.position, step);
+                    if (Vector2.Distance(monster.transform.position, limitedFollow.targetPos.position) <= 0.2f)
+                    {
+                        hitPos1 = true;
+                        limitedFollow.targetPos = monster.GetComponent<LimitedFollow>().limitedPosition2;
+                    }
+                }
+            }
         }
     }
 
@@ -73,7 +144,7 @@ public class EnemyFollowBehavior : MonoBehaviour
 
     private void AnimateIfPlayerClose()
     {
-        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
         if (distanceToPlayer < 2.5f)
         {
             animator.SetBool("PlayerClose", true);
